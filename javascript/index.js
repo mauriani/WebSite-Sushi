01c1227a657ba1840ff83395aec6d57d1967d2f0
Mauriani Maciel <maurianim@gmail.com>
2022-06-13 19:16:28 -0300

    attrs

--- javascript/index.js
+++ javascript/index.js
@@ -1,32 +1,3 @@
-const nav = `
-<a class="logo" src="" alt="logo da empresa">Sushi you again</a>
-
-<ul class="nav-list">
-  <li>
-    <a href="index.html" class="menu-link">Inicio</a>
-  </li>
-
-  <li>
-    <a href="menu.html" class="menu-link">Menu</a>
-  </li>
-
-  <li>
-    <a href="sobre.html" class="menu-link">Sobre</a>
-  </li>
-
-  <li>
-    <a href="contato.html" class="menu-link">Contato</a>
-  </li>
-
-  <li>
-    <a href="cadastro.html" class="menu-link">
-      <i class="fa fa-user-plus"></i>
-    </a>
-  </li>
-</ul>
-`;
-document.getElementById("app-header").innerHTML = nav;
-
 const cadastroConfirmar = document.querySelector("#submit");
 let purecookieTitle = "Cookies.",
   purecookieDesc = "Esse site usa cookie, você aceita usar os cookies?",
